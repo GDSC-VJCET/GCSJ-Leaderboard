fa313bbaed959272a7de3c0b8bc1ed28211911b3
GDSC-VJCET <gdsc@vjcet.org>
2023-10-24 23:39:57 +0530

    initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+  // Fetch data from JSON file
+  $.getJSON('data.json', function(data) {
+    var tableBody = $('#rankings tbody');
+
+    // Iterate over each item in the JSON array
+    $.each(data, function(index, item) {
+      // Append a new row to the table for each item
+      var row = $('<tr>').appendTo(tableBody);
+      row.append('<td>' + item.Rank + '</td>');
+      row.append('<td>' + item.StudentName + '</td>');
+
+      if (item.TotalCompletions === "Yes"){
+        row.addClass('completed');
+      }
+      // Apply alternate row colors
+      if (index % 2 === 0) {
+        row.addClass('even-row');
+      } else {
+        row.addClass('odd-row');
+      }
+    });
+  });
+});
+
+
+  $(document).ready(function() {
+    $('#search-leaderboard').keyup(function() {
+      var searchTerm = $(this).val().toLowerCase();
+      $('#rankings tbody tr').each(function() {
+        var studentName = $(this).find('td:eq(1)').text().toLowerCase();
+        if (studentName.includes(searchTerm)) {
+          $(this).show();
+        } else {
+          $(this).hide();
+        }
+      });
+    });
+  });
